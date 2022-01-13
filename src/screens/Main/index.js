0f347167ecd4bb426e957ed2c77376c26b0a7cbf
1rjun <arjunsumbria@gmail.com>
2022-01-13 12:04:42 +0530

    updated

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -20,11 +20,20 @@ export default class Main extends Component {
                     })
                 }/>
                 <Body selected={this.state.selected}/>
+                <Facebook/>
                 <Footer/>
             </div>
         )
     }
 }
+
+const Facebook = () => 
+    (<div className="container">
+        <hr/>
+        <div class="fb-page" data-href="https://www.facebook.com/AmitMantoOfficial" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AmitMantoOfficial" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AmitMantoOfficial">Amit Manto</a></blockquote></div>
+                {/* <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAmitMantoOfficial&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> */}
+    </div>)
+
 
 
 const Footer = () => {
